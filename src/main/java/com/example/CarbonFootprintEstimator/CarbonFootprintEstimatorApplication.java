619e4bffb2e920489bdccc098272699d8e8de079
Shravya <shravyamende28@gmail.com>
2025-08-31 09:37:58 +0530

    CarbonFootPrint

---
+++ src/main/java/com/example/CarbonFootprintEstimator/CarbonFootprintEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarbonFootprintEstimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonFootprintEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonFootprintEstimatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarbonFootprintEstimator/CarbonFootprintEstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarbonFootprintEstimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonFootprintEstimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
